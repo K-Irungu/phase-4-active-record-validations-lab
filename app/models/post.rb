c12167f38d71e0cb78bc7128dfae498b06384a30
K-Irungu <110017552+K-Irungu@users.noreply.github.com>
2022-11-28 21:27:53 +0300

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+
+    #Custom validator
+    validate :clickbaity_title
+
+    def clickbaity_title
+        if !(["Won't Believe","Secret","Top [number]","Guess"].any?{|phrase|"#{title}".include?(phrase)})
+            errors.add(:title, "not clickbait")
+        end
+    end
 end
